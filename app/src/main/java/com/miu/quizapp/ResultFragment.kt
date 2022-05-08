696d23a757b4ae4e454a0f1416af93431e4f258f
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-05-08 00:41:04 -0400

    bug fix

--- app/src/main/java/com/miu/quizapp/ResultFragment.kt
+++ app/src/main/java/com/miu/quizapp/ResultFragment.kt
@@ -1,11 +1,13 @@
 package com.miu.quizapp
 
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.room.Room
 import com.miu.quizapp.database.Question
 import com.miu.quizapp.database.QuestionDatabase
@@ -13,7 +15,7 @@ import kotlinx.android.synthetic.main.fragment_result.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class ResultFragment : BaseFragment() {
+class ResultFragment : Fragment() {
     lateinit var  db:QuestionDatabase;
     lateinit var qlist:List<Question>;
     var count:Int = 0;
@@ -28,41 +30,40 @@ class ResultFragment : BaseFragment() {
         GlobalScope.launch {
             count = db.questionDao().RowsCount();
             qlist = db.questionDao().GetAllQuestions();
-            GetResult(qlist);
+
+            var correctAnswer:Int = 0;
+            var wrongAnswer:Int = 0;
+            var skippedAnswer:Int = 0;
+            var correction:String = "<b>Answers Correction:</b><br/>";
+            var qIndex = 0;
+            for(q in qlist){
+                if(q.answer == q.correctAnswer){
+                    correctAnswer++;
+                }
+                else if(q.answer != q.correctAnswer && q.answer != -1){
+                    wrongAnswer++;
+
+                    var chekList = q.questionChoices.split("|");
+                   // correction+="<b>Question ${qIndex+1} : <br/>Your selection is : ${chekList[q.answer]}<br/>correct answer is : ${chekList[q.correctAnswer]}</b><br/><br/>"
+                    correction+="Question ${qIndex+1}: Your selection is: ${chekList[q.answer]}, correct answer is :${chekList[q.correctAnswer]}\n"
+                }
+                else if(q.answer == -1){
+                    skippedAnswer++;
+                }
+                qIndex++;
+            }
+
+            tvCorrectAnswers.setText(Html.fromHtml("<b>Total correct answers is : ${correctAnswer}</b>"))
+            tvScore.setText(Html.fromHtml("<b>Your score is : ${correctAnswer}/${count}</b>"));
+            tvSubmittedAnswers.setText(Html.fromHtml("<b>Total submitted answers is : ${correctAnswer+wrongAnswer}/${count}</b>"))
+            tvWrongAnswers.setText(Html.fromHtml("<b>Total wrong answers is : ${wrongAnswer}</b>"))
+         //   tvCorrection.setText(correction)
+
         }
+
         return  view;
     }
 
-    private suspend fun GetResult(QList:List<Question>){
-        var correctAnswer:Int = 0;
-        var wrongAnswer:Int = 0;
-        var skippedAnswer:Int = 0;
-        var correction:String = "";
-        var qIndex = 1;
-        for(q in QList){
-            if(q.answer == q.correctAnswer){
-                correctAnswer++;
-            }
-            else if(q.answer != q.correctAnswer && q.answer != -1){
-                wrongAnswer++;
-
-                var chekList = q.questionChoices.split("|");
-                correction+="Question ${qIndex}: Your selection is: ${chekList[q.answer]}, correct answer is :${chekList[q.correctAnswer]}\n"
-            }
-            else if(q.answer == -1){
-                skippedAnswer++;
-            }
-
-            qIndex++;
 
 
-            Log.i("TEST","Question ${qIndex}: Your selection is: ${q.answer}, correct answer is :${q.correctAnswer}\n")
-        }
-
-        tvCorrectAnswers.text = "Total correct answers is: ${correctAnswer}"
-        tvScore.text = "Your score is ${correctAnswer}/${count}"
-        tvSubmittedAnswers.text = "Total submitted answers ${correctAnswer+wrongAnswer}/${count}";
-        tvWrongAnswers.text ="Total wrong answers is: ${wrongAnswer}"
-        tvCorrection.text = correction
-    }
 }
